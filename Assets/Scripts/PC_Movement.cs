a632e44ecd0d6dc17532f400fbb4f680c55bc425
Dana Kimball <dkimball@ucsd.edu>
2017-01-21 12:36:09 -0800

    Added Basic PC Movement
    Feels a bit stiff and slidy right now.

---
+++ Assets/Scripts/PC_Movement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Movement : MonoBehaviour {
+
+    public float linearSpeed = 10.0f;
+    public float angularSpeed = 180.0f;
+    public float accelerationTime = 0.25f;
+    public float accelerationMod = 5.0f;
+
+    public bool lockRotation = false;
+
+    private Rigidbody2D localbody;
+    private float acceleration;
+
+	// Use this for initialization
+	void Start () {
+        localbody = GetComponent<Rigidbody2D>();
+        acceleration = linearSpeed / accelerationTime;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float hMovement = Input.GetAxis("Horizontal");
+        float vMovement = Input.GetAxis("Vertical");
+
+        Vector2 targetVelocity = new Vector2(hMovement, vMovement).normalized * linearSpeed;
+
+        targetVelocity = Vector2.MoveTowards(localbody.velocity, targetVelocity, Time.deltaTime * acceleration * accelerationMod);
+
+        localbody.velocity = targetVelocity;
+        
+    }
+}
---
+++ Assets/Scripts/PC_Movement2.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PC_Movement2 : MonoBehaviour {
+
+    public float linearSpeed = 10.0f;
+    public float angularSpeed = 180.0f;
+
+    private Rigidbody localbody;
+
+    // Use this for initialization
+    void Start () {
+        localbody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float hMovement = Input.GetAxis("Horizontal");
+        float vMovement = Input.GetAxis("Vertical");
+
+        Vector3 newPos = new Vector3(localbody.position.x, localbody.position.y, localbody.position.z);
+
+        if(hMovement != 0 && vMovement != 0) //this case prevents player from moving faster by going diagonally
+        {
+            newPos.x += hMovement * linearSpeed * Time.deltaTime / Mathf.Sqrt(2);
+            newPos.z += vMovement * linearSpeed * Time.deltaTime / Mathf.Sqrt(2);
+            localbody.MovePosition(newPos);
+        } else if(hMovement != 0 || vMovement != 0)
+        {
+            newPos.x += hMovement * linearSpeed * Time.deltaTime;
+            newPos.z += vMovement * linearSpeed * Time.deltaTime;
+            localbody.MovePosition(newPos);
+        }
+    }
+}
