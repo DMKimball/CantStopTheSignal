6e704f38c2a28ffa366085697b59df3b95af7751
Stephen Trinh <s6trinh@ucsd.edu>
2017-01-21 18:08:37 -0800

    Stephen's 'merge'

---
+++ Assets/Scripts/CircleMeshGenerator.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
+public class CircleMeshGenerator : MonoBehaviour
+{
+    public float radius = 10f;
+    public int segments = 60;
+    public bool CreateNewMaterial = false;
+    public Material material;
+}
---
+++ Assets/Scripts/LineRendererDottedLine.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent (typeof(LineRenderer))]
+public class LineRendererDottedLine : MonoBehaviour
+{
+    public Transform startPosition;
+    public Transform endPosition;
+    public Material lineMaterial;
+
+    private LineRenderer _lineRenderer;
+
+    // Use this for initialization
+    void Start()
+    {
+        _lineRenderer = GetComponent<LineRenderer>();
+        SetupLine();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SetupLine();
+    }
+
+    void SetupLine()
+    {
+        _lineRenderer.sortingLayerName = "OnTop";
+        _lineRenderer.sortingOrder = 5;
+        _lineRenderer.numPositions = 2;
+        _lineRenderer.SetPosition(0, startPosition.position);
+        _lineRenderer.SetPosition(1, endPosition.position);
+        _lineRenderer.startWidth = 0.5f;
+        _lineRenderer.endWidth = 0.5f;
+        _lineRenderer.useWorldSpace = false;
+        _lineRenderer.material = lineMaterial;
+    }
+}
