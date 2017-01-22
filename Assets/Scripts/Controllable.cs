aa231ce2414f1374689c3fd7597fcde15ebb05f3
Dana Kimball <dkimball@ucsd.edu>
2017-01-22 12:46:47 -0800

    EvilAI mostly functional

--- Assets/Scripts/Controllable.cs
+++ Assets/Scripts/Controllable.cs
@@ -4,8 +4,11 @@ using UnityEngine;
 
 public class Controllable : MonoBehaviour, Interactable {
 
+    public bool secure = false;
+
 	public void Interact(GameObject interactor)
     {
+        if (secure) return;
         SwitchControl controller = interactor.GetComponentInChildren<SwitchControl>();
         controller.PassControl(gameObject);
     }
